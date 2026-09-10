59877188d7fc7f437a5c55d94e1b0175531d3c25
apple <apple@apples-MacBook-Air.local>
2026-09-10 21:42:20 +0300

    Fix TypeScript props type error in ModelViewer component

--- components/ModelViewer.tsx
+++ components/ModelViewer.tsx
@@ -4,30 +4,32 @@ import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Center, OrbitControls, useGLTF } from '@react-three/drei';
 
-function ModelContent() {
-  const { scene } = useGLTF('/pencil.glb');
+interface ModelViewerProps {
+  modelPath: string;
+}
+
+function ModelContent({ modelPath }: { modelPath: string }) {
+  const { scene } = useGLTF(modelPath);
 
   return (
     <Center>
       <primitive 
         object={scene} 
-        scale={0.8}          // Reduced scale size for quicker rendering and optimal fit
-        position={[0, 0, 0]}   // Centers the model precisely
+        scale={0.8} 
+        position={[0, 0, 0]} 
       />
     </Center>
   );
 }
 
-useGLTF.preload('/pencil.glb');
-
-export default function ModelViewer() {
+export default function ModelViewer({ modelPath }: ModelViewerProps) {
   return (
     <div className="w-full h-full min-h-[200px]">
       <Canvas camera={{ position: [0, 0, 4], fov: 45 }}>
         <ambientLight intensity={2.0} />
         <directionalLight position={[5, 5, 5]} intensity={2.5} />
         
-        <ModelContent />
+        <ModelContent modelPath={modelPath} />
         
         <OrbitControls enableZoom={false} autoRotate autoRotateSpeed={1.0} />
       </Canvas>
